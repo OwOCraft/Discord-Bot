323b9c8c249e50cf50482dd5153ca8ea4d24210d
logzinga <65839350+logzinga@users.noreply.github.com>
2022-12-30 00:33:06 +1100

    slash commands should start to work

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
